25b89ac7574b97c365b1f21001198402b86d2ec7
Othman Ahmed <othmanahmed258@gmail.com>
2026-01-30 00:58:20 +0200

    edit: links in Header.jsx component

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { FiMenu } from "react-icons/fi";
+import { links } from "@/lib/links";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -12,24 +13,14 @@ const Header = () => {
       <header className="fixed top-0 left-0 right-0 mx-auto mt-[2rem] flex justify-center z-[100]">
         <nav className="hidden md:block">
           <ul className="flex gap-[2rem] font-[500] shadow-lg px-[1.8rem] py-[.7rem] bg-white/40 backdrop-blur-sm w-fit rounded-full border outline-none">
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#">Home</Link>
-            </li>
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#about">About</Link>
-            </li>
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#skills">Skills</Link>
-            </li>
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#qualification">Experience</Link>
-            </li>
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#work">Work</Link>
-            </li>
-            <li className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800">
-              <Link href="#contact">Contact</Link>
-            </li>
+            {links.map((link) => (
+              <li
+                key={link.id}
+                className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
+              >
+                <Link href={`#${link.path}`}>{link.name}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
@@ -50,54 +41,17 @@ const Header = () => {
           {toggle && (
             <>
               <ul className="flex flex-col items-center gap-[2rem] absolute font-[500] top-[5rem] transform translate-x-[-43%] shadow-lg px-[5rem] py-[1.5rem] bg-white/40 backdrop-blur-sm w-fit rounded-[.8rem] border outline-none">
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#">Home</Link>
-                </li>
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#about">About</Link>
-                </li>
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#skills">Skills</Link>
-                </li>
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#qualification">Qualification</Link>
-                </li>
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#work">Work</Link>
-                </li>
-                <li
-                  onClick={() => {
-                    setToggle(false);
-                  }}
-                  className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
-                >
-                  <Link href="#contact">Contact</Link>
-                </li>
+                {links.map((link) => (
+                  <li
+                    key={link.id}
+                    onClick={() => {
+                      setToggle(false);
+                    }}
+                    className="transition duration-300 ease-in text-neutral-400 hover:text-neutral-800"
+                  >
+                    <Link href={`#${link.path}`}>{link.name}</Link>
+                  </li>
+                ))}
               </ul>
             </>
           )}
